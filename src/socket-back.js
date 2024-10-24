2fe1a5d663f3c28b8e1e115177c30fe830af7914
Ariel Spencer <arielspencer.tech@gmail.com>
2024-10-23 23:58:32 -0300

    feat(socket): implement text update broadcasting

--- src/socket-back.js
+++ src/socket-back.js
@@ -2,4 +2,8 @@ import io from "./servidor.js";
 
 io.on('connection', (socket) => {
   console.log('Um usuário conectado! ID:', socket.id);
+
+  socket.on('textoAtualizado', (texto) => {
+    console.log('Texto atualizado:', texto);
+  });
 });
