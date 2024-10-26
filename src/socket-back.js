3f268ff3fb1bd642b04efcf23222930fb226b4a8
Ariel Spencer <arielspencer.tech@gmail.com>
2024-10-26 15:29:31 -0300

    feat(socket): add document selection and retrieval functionality

--- src/socket-back.js
+++ src/socket-back.js
@@ -1,9 +1,28 @@
 import io from "./servidor.js";
 
+const documentos = [
+  {
+    nome: "JavaScript",
+    texto: "texto de JavaScript..."
+  },
+  {
+    nome: "Node",
+    texto: "texto de Node..."
+  },
+  {
+    nome: "Socket.io",
+    texto: "texto de Socket.io..."
+  }
+];
+
 io.on('connection', (socket) => {
   console.log('Um usuário conectado! ID:', socket.id);
 
   socket.on('selecionar_documento', (nomeDocumento) => {
+    const documento = encontrarDocumento(nomeDocumento);
+
+    console.log('Documento selecionado:', documento);
+
     socket.join(nomeDocumento);
   });
 
@@ -16,3 +35,11 @@ io.on('connection', (socket) => {
     Motivo: ${motivo}`);
   });
 });
+
+function encontrarDocumento(nome) {
+  const documento = documentos.find((documento) => {
+    return documento.nome === nome;
+  });
+
+  return documento
+}
